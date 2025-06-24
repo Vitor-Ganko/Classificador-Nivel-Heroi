9fce32890ea305f763adcb86f0fff143f7903c2a
Vitor-Ganko <vitorganko@gmail.com>
2025-06-24 14:02:09 -0300

    Codificando as condições e a saída do classificador de nível do herói

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 let heroi = ["Ganksmann", 0]
 let nivelHeroi = null;
 
+if (heroi[1] <= 1000) nivelHeroi = "Ferro"
+else if (heroi[1] > 1000 && heroi[1] <= 2000) nivelHeroi = "Bronze"
+else if (heroi[1] > 2000 && heroi[1] <= 3000) nivelHeroi = "Prata"
+else if (heroi[1] > 3000 && heroi[1] <= 4000) nivelHeroi = "Ouro"
+else if (heroi[1] > 4000 && heroi[1] <= 5000) nivelHeroi = "Platina"
+else if (heroi[1] > 5000 && heroi[1] <= 6000) nivelHeroi = "Diamante"
+else if (heroi[1] > 6000 && heroi[1] <= 8000) nivelHeroi = "Ascendente"
+else if (heroi[1] > 8000 && heroi[1] < 10000) nivelHeroi = "Imortal"
+else if (heroi[1] > 10000) nivelHeroi = "Radiante"
+
+console.log("O herói de nome " + heroi[0] + " está no nível " + "'" + nivelHeroi + "'")
